9843add03083ede6cddabc4828d92dea8171eef0
tienhai912 <tienhai912@yahoo.com.vn>
2017-06-29 11:51:17 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "rails_sample_app"
+    page_title.empty? ? base_title : page_title + t("vertical_bar") + base_title
+  end
 end

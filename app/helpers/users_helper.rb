73e0e99a02f2f07954b6a1a1054593c5d624d20b
tienhai912 <tienhai912@yahoo.com.vn>
2017-07-10 09:18:07 +0700

    Finish chapter 10

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
-  def gravatar_for
-    gravatar_id = Digest::MD5.hexdigest @user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag gravatar_url, alt: @user.name, class: "gravatar"
+  def gravatar_for user, options = {size: Settings.user.default_gravatar_size}
+    @temp_user = user
+    gravatar_id = Digest::MD5.hexdigest @temp_user.email.downcase
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag gravatar_url, alt: @temp_user.name, class: "gravatar"
   end
 end
